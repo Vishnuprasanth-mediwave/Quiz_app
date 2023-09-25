17e1bdc0dcc95818967a5a3424d5667c5520f0af
Vishnuprasanth-mediwave <vishnu_prasanth@mindwaveventures.com>
2023-09-25 15:41:46 +0530

    second commit

--- app/script.js
+++ app/script.js
@@ -1,5 +1,44 @@
-const selectElement = document.getElementById("category");
+// const state = {
+//   categories: [
+//     {
+//       id: 1234,
+//       name: "Computer Science",
+//     },
+//     {
+//       id: 2345,
+//       name: "Geology",
+//     },
+//   ],
+//   questions: [
+//     {
+//       id: 300,
+//       title: "What color is the sky?",
+//       options: [
+//         { id: 10, text: "Blue", isCorrect: true },
+//         { id: 20, text: "Red", isCorrect: false },
+//         { id: 30, text: "Green", isCorrect: false },
+//       ],
+//       category: 2345,
+//     },
+//     {
+//       id: 301,
+//       title: "Which is a CPU company?",
+//       options: [
+//         { id: 10, text: "Intel", isCorrect: false },
+//         { id: 20, text: "AMD", isCorrect: false },
+//         { id: 30, text: "all the above", isCorrect: true },
+//       ],
+//       category: 1234,
+//     },
+//   ],
+//   // page: "index", // 'question'
+// };
 
+
+const selectElement = document.getElementById("category");
+let selectedValue = "";
+const quiz = document.querySelector("#quiz");
+quiz.style.display = "none";
 const category = [
   {
     name: "Computer science",
@@ -10,15 +49,197 @@ const category = [
     value: "bio",
   },
 ];
-for (let sub of category){
-    const option=document.createElement("option")
-    option.value=sub.value;
-    option.textContent=sub.name;
+for (let sub of category) {
+  const option = document.createElement("option");
+  option.value = sub.value;
+  option.textContent = sub.name;
 
-    selectElement.appendChild(option)
+  selectElement.appendChild(option);
 }
 // Get the selected value and move to the respected page when the button is clicked
 document.getElementById("proceed").addEventListener("click", function () {
-  var selectedValue = selectElement.value;
-  window.location.href = `question.html?type=${selectedValue}`;
+  selectedValue = selectElement.value;
+  const container = document.querySelector(".container");
+  container.style.display = "none";
+  const quiz = document.querySelector("#quiz");
+  quiz.style.display = "block";
+  clearContent();
+  appendToContent();
+  appendToButton();
+  updateUiList(selectedValue);
 });
+//----------------------------------------------------------------------
+// function callQuestion(value){
+
+const computer = [
+  {
+    id: "1",
+    question: "Who is the father of Computer science?",
+    options: ["Charles Babbage", "right brothers", "william", "livingston"],
+    correctAns: "Charles Babbage",
+  },
+  {
+    id: "2",
+    question: "In a computer, most processing takes place in _______?",
+    options: ["CPU", "mouse", "keyboard", "monitor"],
+    correctAns: "CPU",
+  },
+  {
+    id: "3",
+    question: "Scientific Name of Computer?",
+    options: ["Sillico sapiens", "Hybrid Computer", "Interpreter", "comdpromt"],
+    correctAns: "Sillico sapiens",
+  },
+];
+
+const bio = [
+  {
+    id: "1",
+    question: "The human heart is ",
+    options: [" Neurogenic heart", "Myogenic heart", "Ampullary", "Pulsating"],
+    correctAns: "Myogenic heart",
+  },
+  {
+    id: "2",
+    question: "Spermology is the study of ",
+    options: ["Seed", "Leaf", "Fruit", "Pollen"],
+    correctAns: "Seed",
+  },
+  {
+    id: "3",
+    question: "Who is known as father of Zoology ",
+    options: ["Darwin", "Aristotle", "Aristotle", "Theophrastus"],
+    correctAns: "Aristotle",
+  },
+];
+
+const questionCollection = {
+  computer: computer,
+  bio: bio,
+};
+
+function updateUiList(value) {
+  
+  const app = document.querySelector("#app");
+  for (let mcq of questionCollection[value]) {
+    const event = MakeQuestionList(mcq);
+    app.appendChild(event);
+  }
+  const heading = document.querySelector("#topic");
+  heading.innerHTML = "welcome to quiz of " + value;
+}
+function clearContent() {
+  const content = document.querySelector("#content");
+   content.innerHTML=""
+}
+
+// const urlParams = new URLSearchParams(window.location.search);
+// const myType = urlParams.get("type");
+
+// console.log(questionCollection[myType]);
+
+function MakeQuestionList(mcq) {
+  const div = document.createElement("div");
+  div.setAttribute("class", "question-container");
+  div.setAttribute("id", `question-${mcq["id"]}`);
+
+  const questionDiv = document.createElement("div");
+  questionDiv.setAttribute("class", "question");
+
+  const questionParagraph = document.createElement("p");
+  questionParagraph.textContent = mcq["question"];
+  questionDiv.appendChild(questionParagraph);
+
+  const optionDiv = document.createElement("div");
+  optionDiv.setAttribute("class", "options");
+  optionDiv.setAttribute("id", "options");
+
+  for (let i = 0; i < mcq.options.length; i++) {
+    const label = document.createElement("label");
+
+    const inputRadio = document.createElement("input");
+    inputRadio.setAttribute("type", "radio");
+    inputRadio.setAttribute("id", `radio-${mcq["id"]}`);
+    inputRadio.setAttribute("name", `answer-${mcq["id"]}`);
+    inputRadio.value = mcq.options[i];
+
+    label.appendChild(inputRadio);
+    label.appendChild(document.createTextNode(mcq.options[i]));
+    optionDiv.appendChild(label);
+  }
+  const resultdiv = document.createElement("div");
+  const resultId = `result-${mcq["id"]}`;
+  resultdiv.setAttribute("id", resultId);
+  resultdiv.className = "ans-div";
+
+  div.appendChild(questionDiv);
+  div.appendChild(optionDiv);
+  div.appendChild(resultdiv);
+
+  const submit = document.querySelector("#submitBtn");
+  submit.addEventListener("click", function () {
+    const selected = document.querySelector(
+      `input[name="answer-${mcq.id}"]:checked`
+    );
+
+    const checkAnswer = mcq.correctAns;
+    if (selected) {
+      const selectedAnswer = selected.value;
+      if (checkAnswer == selectedAnswer) {
+        correctAnsShow(checkAnswer, `${mcq["id"]}`, "green");
+      } else {
+        correctAnsShow(checkAnswer, `${mcq["id"]}`, "red");
+      }
+    } else {
+      correctAnsShow(checkAnswer, `${mcq["id"]}`, "orange");
+    }
+  });
+
+  // findNotAnswering(`result-${mcq["id"]}`);
+
+  return div;
+}
+function correctAnsShow(ans, resultId, add) {
+  const divId = `#question-${resultId}`;
+  const divClass = "border-" + add;
+  const div = document.querySelector(divId);
+  div.classList.add(divClass);
+  const selector = `#result-${resultId}`;
+  const result = document.querySelector(selector);
+  result.innerHTML = "Ans: " + ans;
+  result.classList.add(add);
+}
+
+function appendToButton() {
+  const buttondiv = document.querySelector("#buttons");
+
+  const submit = document.createElement("button");
+  submit.id = "submitBtn";
+  submit.innerHTML = "submit";
+  const back = document.createElement("button");
+  back.id = "backBtn";
+  back.innerHTML = "Back";
+
+  buttondiv.appendChild(back);
+  buttondiv.appendChild(submit);
+
+  back.addEventListener("click", () => {
+    const container = document.querySelector(".container");
+    const quiz = document.querySelector("#quiz");
+    quiz.style.display = "none";
+    container.style.display = "block";
+  });
+}
+function appendToContent() {
+  const content=document.querySelector("#content")
+  const appDiv = document.createElement("div");
+  appDiv.id = "app";
+
+  // Create another div with the id "buttons"
+  const buttonsDiv = document.createElement("div");
+  buttonsDiv.id = "buttons";
+
+  // Append the created div elements to the document's body or another parent element
+  content.appendChild(appDiv);
+  content.appendChild(buttonsDiv);
+}
